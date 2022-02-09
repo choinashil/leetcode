fd85197325aa8e22bad7edd813c2f36e524e001a
Nashil <choinashil@gmail.com>
2022-02-10 00:36:22 +0900

    Time: 82 ms (61.42%), Space: 42.4 MB (17.35%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  const profits = [];
+   let profit = 0;
   
   for (let i = 1; i < prices.length; i++) {
-    const profit = prices[i] - prices[i - 1];
-    if (profit > 0) {
-      profits.push(profit);
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
     }
   }
 
-  return profits.length ? profits.reduce((a, b) => a + b) : 0;
+  return profit;
 };
