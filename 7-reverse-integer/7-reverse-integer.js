a26dc9eacac2c722440f37768455f621b7ab1b70
Nashil <choinashil@gmail.com>
2022-02-10 01:07:05 +0900

    Time: 76 ms (96.28%), Space: 44 MB (15.14%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.js
+++ 7-reverse-integer/7-reverse-integer.js
@@ -3,19 +3,12 @@
  * @return {number}
  */
 var reverse = function(x) {
-  let original = '' + Math.abs(x);
-  let result = '';
   const isPositive = x > 0;
+  const reversed = +(Math.abs(x) + '').split('').reverse().join('');
   
-  while (original.length) {
-    const sliced = original % 10;
-    result += sliced;
-    original = original.slice(0, -1);
-  }
-  
-  if (result < -Math.pow(2, 31) || result > Math.pow(2, 31) - 1) {
+  if (reversed < -Math.pow(2, 31) || reversed > Math.pow(2, 31) - 1) {
     return 0;
   }
 
-  return isPositive ? +result : -result;
+  return isPositive ? reversed : -reversed;
 };
