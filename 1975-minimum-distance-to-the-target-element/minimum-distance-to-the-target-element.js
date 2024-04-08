b7d4b5b2e35794c6beaeddcab5dba40795464567
Nashil <choinashil@gmail.com>
2024-04-08 23:37:57 +0900

    Time: 55 ms (45.19%) | Memory: 48.9 MB (77.88%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+var getMinDistance = function(nums, target, start) {
+    let min = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            const result = Math.abs(i - start);
+
+            if (min > result) {
+                min = result;
+            }
+        }
+    }
+
+    return min;
+};
