a63f1f5bf8dd45d9e5fc82178ea5e37d5cf35e4e
Nashil <choinashil@gmail.com>
2024-04-17 16:03:18 +0900

    Time: 223 ms (46.17%) | Memory: 72.6 MB (76.94%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -4,33 +4,15 @@
  */
 var dailyTemperatures = function(temperatures) {
     const stack = [];
-    const result = [];
+    const result = new Array(temperatures.length).fill(0);
 
     for (let i = 0; i < temperatures.length; i++) {
-        if (i === temperatures.length - 1) {
-            while (stack.at(-1)) {
-                const lastItem = stack.pop();
-
-                if (lastItem.temperature < temperatures[i + 1]) {
-                    result[lastItem.index] = i + 1 - lastItem.index;
-                } else {
-                    result[lastItem.index] = 0;
-                }
-            }
-            
-            result.push(0);
+        while (stack.length && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+            const index = stack.pop();
+            result[index] = i - index;
         }
 
-        if (temperatures[i] < temperatures[i + 1]) {
-            result[i] = 1;
-
-            while (stack.at(-1) !== undefined && stack.at(-1).temperature < temperatures[i + 1]) {
-                const lastItem = stack.pop();
-                result[lastItem.index] = i + 1 - lastItem.index;
-            }
-        } else {
-            stack.push({ temperature: temperatures[i], index: i });
-        }
+        stack.push(i);
     }
 
     return result;
