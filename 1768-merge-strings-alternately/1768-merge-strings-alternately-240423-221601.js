fdded92b64b30462d328e551026d50a7e654ac68
Nashil <choinashil@gmail.com>
2024-04-23 22:16:08 +0900

    Time: 48 ms (84.88%), Space: 49528000 (50.62%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately-240423-221601.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let result = '';
+
+    const length = Math.max(word1.length, word2.length);
+
+    for (let i = 0; i < length; i++) {
+      result += word1[i] ?? '';
+      result += word2[i] ?? '';
+    }
+
+    return result;
+};
